b53aa23984343edb8b50bcb9eacd5f9c459a84c5
Will <silentwill29@gmail.com>
2024-12-15 17:57:17 +1100

    Added campaign player controller class

---
+++ Source/ActionAdventure/CampaignPlayerController.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CampaignPlayerController.generated.h"
+
+UCLASS()
+class ACTIONADVENTURE_API ACampaignPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
